d5b9fe165a064c5c8848b380b1a465325f927fe6
Jerry Evans <g4@novadsp.com>
2014-02-07 15:51:15 +0000

    Remove all uvopt files. Add config.h

---
+++ examples/keil/NGX-4330/USB/Audio/config.h
@@ -0,0 +1,32 @@
+/*
+
+	Config file for commonly modified settings
+
+*/
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+//#define CHANNEL_COUNT 32
+#define CHANNEL_COUNT 6
+// set to 1 to use channel mask, set to 0 to zero it out
+#define USE_CHANNEL_MASK 1
+
+// JME this is channel independent
+// do not change for purposes of resolving channel mask issue
+#define SAMPLE_COUNT 8
+
+// valid sizes are 2 and 3 for 16 and 24 bit respectively
+// do not change for purposes of resolving channel mask issue
+#define BYTES_PER_SAMPLE 2
+
+// sets the isoch endpoint size
+#define EP_SIZE_BYTES 1024
+
+// set this to 1 to experiment with device controls
+// like volume etc.
+// Do not modify. This will break things/
+#define USE_FEATURE 0
+
+#endif
+
